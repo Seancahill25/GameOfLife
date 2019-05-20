b5448dec12d08893631face4d40f86c75d5a3d49
Sean Cahill <seancahill25@gmail.com>
2019-05-20 18:49:31 -0400

    first commit

---
+++ GameOfLifeWebApplication/Models/Grid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameOfLifeWebApplication.Models
+{
+    public class Grid
+    {
+        public bool[,] grid;
+        public int generation = 0;
+
+        public Grid(int rows, int columns)
+        {
+            this.grid = new bool[rows, columns];
+
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j < columns; j++)
+                {
+                    this.grid[i, j] = false;
+                }
+            }
+        }
+    }
+}
